fef678028a29ecd268ae4be42a7acce33c48ae4c
lpbaiser <lpbaiser@gmail.com>
2020-06-21 11:20:31 -0300

    Removed lib "net", fixed export function parse;

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
-const net				= require('net');
-const gt06 = require('./gt06n.js');
+const net = require('net');
+const { parse } = require('./gt06n.js');
 
 const server = net.createServer((connection) => {
     connection.setEncoding('hex');
     connection.on('data', (data) => {
-        let result = gt06.parse(data);
-
+        let result = parse(data);
+        console.log('result', result);
     });
-}).listen(8090);
+});
+
+server.on('error', (err) => {
+    throw err;
+});
+server.listen(9000, () => {
+    console.log('server listen port: 9000');
+});
 
